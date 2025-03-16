0ed5c722b9176bfba0c577075bc4fa8165e84adb
Cade England <cadejengland@proton.me>
2025-03-15 21:56:42 -0500

    changed method to calculate using factors

--- python/P005.py
+++ python/P005.py
@@ -7,23 +7,56 @@ What is the smallest positive number that is evenly divisible by all of the numb
 """
 
 
-def divisible(x: int) -> bool:
+def get_factors(n: int) -> list[int]:
+    # include repeating factors
 
-    for n in range(20, 1, -1):
-        if x % n != 0:
-            return False
-    return True
+    l = []
+    i = 2
+    while i <= n:
+        if n % i == 0:
+            l.append(i)
+            n /= i
+        else:
+            i += 1
+    return l
+
+
+def solve_for_n(n: int) -> int:
+
+    factor_count_max = {}
+
+    for x in range(n, 1, -1):
+        factors = get_factors(x)
+
+        factor_count = {}
+        # iterate and count occurences
+        for factor in factors:
+            if factor not in factor_count:
+                factor_count[factor] = 1
+            else:
+                factor_count[factor] += 1
+
+        # compare to outer dict
+        for k, v in factor_count.items():
+            if k not in factor_count_max:
+                factor_count_max[k] = v
+            else:
+                if factor_count_max[k] < v:
+                    factor_count_max[k] = v
+
+    # calculate
+    result = 1
+    for k, v in factor_count_max.items():
+        result *= k**v
+
+    return result
 
 
 def main():
+    # if you get the prime factors of 1-20 and multiply the highest powers, you get the answer
+    # can we make a program that will calculate this for any range?
 
-    x = 20  # starting with something arbitrary
-    while True:
-        x += 20
-
-        if divisible(x):
-            print(x)
-            break
+    print(solve_for_n(20))
 
 
 if __name__ == "__main__":
